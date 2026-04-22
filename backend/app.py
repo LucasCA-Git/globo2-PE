4b7b5ea58d383f41fbed887969ad2238fa233a11
David Jeremias <davidojeremias@gmail.com>
2026-04-21 21:36:32 -0300

    fix: docker-compose e configuração do Flask para rodar em container

--- backend/app.py
+++ backend/app.py
@@ -12,4 +12,4 @@ app.register_blueprint(dashboard_bp)
 app.register_blueprint(event_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
